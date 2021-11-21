33230716b21e93546ae4423b196eeb03468627e8
Tichalo <ticha.muma@chatdesk.com>
2021-11-21 02:33:20 +0100

    delete files

--- shopSql/controllers/error.js
+++
@@ -1,6 +0,0 @@
-
-
-exports.get404 = (req, res) => {
-    // res.status(404).sendFile((path.join(__dirname, 'views', '404.html')))
-    res.render('404', {pageTitle: 'Page Not Found', path: '/404'})
-}
--- shopSql/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('shop', 'root', 'mumalo1993', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
